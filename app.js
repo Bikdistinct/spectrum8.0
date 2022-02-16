9d7a6257ff6dae8c1610b63286dcab1f1f29c502
Bikramjit <bikdev07@gmail.com>
2022-02-16 17:20:08 +0530

    heroku

---
+++ app.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const ejs=require("ejs");
+const app=express();
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.render("home2");
+})
+
+app.get("/module",function(req,res){
+    res.render("home3");
+
+});
+
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("started server");
+});
