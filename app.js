b219aacbdac6999c46f16e89956e26c7352feec3
Bikramjit <bikdev07@gmail.com>
2022-02-20 01:28:37 +0530

    final1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set("view engine","ejs");
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-    res.render("Index");
+    res.render("index");
 })
 
 app.get("/Technical",function(req,res){
